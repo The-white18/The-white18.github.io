2d84a9e5eced042b1ef81c819ccf3192d96f28b5
Omar <broli1017@gmail.com>
2023-11-16 08:26:35 -0500

    actualice

---
+++ conexion.php
@@ -0,0 +1,23 @@
+<html> 
+   <body>       
+         <?php
+
+              echo"Hola! Este es mi primer script<br>";
+              #hola 
+              /* La profesora 
+              paola es la mejor*/
+              //Práctica Omar Vega
+              $vartext = "Omar Vega";
+              echo"$vartext <br> <br>"; 
+              $varnumber1 = 15;
+              $varnumber2 = 10;
+              $varnumber3 = $varnumber1 + $varnumber2;
+              $varnumber4 = $varnumber1 * $varnumber2;
+              $varnumber5 = $varnumber1 - $varnumber2;
+              echo"Este es un ejemplo de operaciones aritmeticas: <br>";
+              echo"La suma de $varnumber1 + $varnumber2 es = $varnumber3 <br>";
+              echo"La multiplicación de $varnumber1 * $varnumber2 es = $varnumber4 <br>";
+              echo"La resta de $varnumber1 - $varnumber2 es = $varnumber5 <br>";
+         ?>
+   </body>
+</html>
